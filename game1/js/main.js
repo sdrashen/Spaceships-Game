3e4bb7b16ce4656b38a34a2a92c0500c49b3d896
Sabrina Drashen <sdrashen@gmail.com>
2021-12-17 20:22:29 -0300

    Trying to move the black helicopter

--- game1/js/main.js
+++ game1/js/main.js
@@ -11,15 +11,36 @@ function start() {
     $('#gameBackground').append("<div id='inimigo2'></div>")
     $('#gameBackground').append("<div id='amigo' class='anima3'></div>")
 
+    //Main vars of the game
     const game = {}
+    const KEY = {
+        //In this var we create some definitions
+        W: 13,
+        S: 01,
+        D: 02
+    }
+
+    game.pressed = [] //This is a var type array
+
+    // //The code bellow check if the player pressed a key. We do that by using keydown
+
+    $(document).keydown(function(e) {
+            game.pressed[e.which] = true
+        })
+        //     //keyup measn that no key is being pressed
+    $(document).keyup(function(e) {
+            game.pressed[e.which] = false
+        })
+        //To identify which key was pressed we use a function that will be called in the gameloop
 
     //Game Loop is a function that will run constantly and inside this function we also may call other functions that should run constantly
     //If game loop stops so will the game
     game.timer = setInterval(loop, 30) //Here we're calling loop function each 30ms
-        //timer is a property. setIntervel is a time comand where we indicate the function and the running time.
+        //Here we created a property (timer) to the game var. setIntervel is a time comand where we indicate the function and the running time.
 
     function loop() {
         movebackground()
+        moveplayer()
     }
 
     //This function moves the background of the game
@@ -33,6 +54,24 @@ function start() {
         $('#gameBackground').css('background-position', left - 1)
             //As the loop function will run every 30ms, 1px will always be subtracted the background will always be moving
             //We can change the velocity of the moving by changing the -1
+    }
+
+    function moveplayer() {
+        if (game.pressed[KEY.W]) {
+            const topo = parseInt($('#player').css('top'))
+                //Taking the css value of the top div. For example, if this div has value of 20 then the var top will value 20
+            $('#player').css('top', topo - 10) //Here we update the same player div in the to property with the value of 20 - 10
+                //So the nave will rise 10 unites up
+        }
+
+        if (game.pressed[KEY.S]) {
+            const topo = parseInt($('#player').css('top'))
+            $('#player').css('top', topo + 10)
+        }
+
+        // if (game.pressed[KEY.D]) {
+        //     //Chama fun��o Disparo
+        // }
     }
 }
 //end of function start
